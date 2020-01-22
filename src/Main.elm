fb747c8e5432642c56563b2319972585054df080
Charlie Bevis <charliebevis@gmail.com>
2020-01-21 20:04:27 -0600

    add a button that does nothing

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Browser
 import Element exposing (centerY, fill, padding, rgb, spacing, width)
 import Element.Background exposing (color)
+import Element.Input as Input
 import Html exposing (Html, text, div, h1, img)
 import Html.Attributes exposing (src)
 
@@ -39,9 +40,12 @@ update msg model =
 view : Model -> Html Msg
 view model =
     Element.layout [] <|
-        Element.el
+        Element.column
             [width fill, centerY, color (rgb 0.8 0.4 0.4), padding 30]
-         (Element.text "hello from elm-ui")
+         [ Element.text "hello from elm-ui",
+          Input.button [color (rgb 0.1 0.8 0.8), padding 10]
+           {onPress=Nothing, label=Element.text "make a request"}
+          ]
 
 
 
